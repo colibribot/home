45b69a243f0c7fde779dd7fe1dea1759f79aa24f
Colibri <alex1061.business@gmail.com>
2024-06-07 16:04:44 +0200

    Create script.js
    Signed-off-by: Colibri <alex1061.business@gmail.com>

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("profile-pic").addEventListener("click", function() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.profile-pic')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
