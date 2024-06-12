b4523cd1f39951a6ba608f6859c1e7b365e5bce0
Colibri <alex1061.business@gmail.com>
2024-06-12 19:46:37 +0200

    Update script.js
    Signed-off-by: Colibri <alex1061.business@gmail.com>

--- script.js
+++ script.js
@@ -76,8 +76,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     initialize();
 
+    profilePic.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.closest('.profile')) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+
     hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('open');
-        hamburger.classList.toggle('toggle');
+        navLinks.classList.toggle('active');
     });
 });
