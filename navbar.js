aa75ba29ddf14cd493b0f5da79e048f0028ebba1
Colibri <alex1061.business@gmail.com>
2024-07-30 14:40:55 +0200

    Update navbar.js
    Signed-off-by: Colibri <alex1061.business@gmail.com>

--- navbar.js
+++ navbar.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const profilePic = document.querySelector('.profile-pic');
+    const profileDropdown = document.querySelector('.profile-dropdown');
     const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.navbar-links');
+    const navbarLinks = document.querySelector('.navbar-links');
+
+    profilePic.addEventListener('click', () => {
+        profileDropdown.style.display = profileDropdown.style.display === 'block' ? 'none' : 'block';
+    });
+
+    window.addEventListener('click', (e) => {
+        if (!e.target.closest('.navbar-profile')) {
+            profileDropdown.style.display = 'none';
+        }
+    });
 
     hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('open');
-        hamburger.classList.toggle('toggle');
+        hamburger.classList.toggle('open');
+        navbarLinks.classList.toggle('open');
     });
 });
