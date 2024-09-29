47663b72b7e25b211f5013138afb49e07b4ee27d
Colibri <alex1061.business@gmail.com>
2024-09-29 17:36:40 +0200

    Update loadFooter.js
    Signed-off-by: Colibri <alex1061.business@gmail.com>

--- loadFooter.js
+++ loadFooter.js
@@ -2,9 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loadFooter = async () => {
         try {
-            const response = await fetch('footer.html');
+            const response = await fetch('/home/footer.html');
             if (response.ok) {
                 const footerHTML = await response.text();
+                document.body.insertAdjacentHTML('beforeend', footerHTML);
             } else {
                 console.error('Failed to load footer:', response.status);
             }
